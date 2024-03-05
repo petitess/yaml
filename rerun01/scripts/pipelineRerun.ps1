0aef97453f7d0eb354543434ad29239952cbdf1d
Karol <93782725+petitess@users.noreply.github.com>
2024-03-05 07:45:53 +0100

    rerun failed pipeline

--- rerun01/scripts/pipelineRerun.ps1
+++ rerun01/scripts/pipelineRerun.ps1
@@ -1,7 +1,7 @@
 $Runs = az pipelines runs list --org 'https://dev.azure.com/xxx' --result 'failed' --project 'Operations' --query "[?definition.name=='opsgenie' && contains(startTime, '$(Get-Date -Format "yyyy-MM-dd")')]" 
 ($Runs | ConvertFrom-Json)[0]
 ($Runs | ConvertFrom-Json) | ForEach-Object {
-    az pipelines run --project 'Operations' --branch 'main' --id $_.definition.id
+    az pipelines run --org 'https://dev.azure.com/xxx' --project 'Operations' --branch 'main' --id $_.definition.id
     Write-Host "Started pipeline: $($_.definition.id)"
 }
 
