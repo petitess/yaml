ebed20664b397f7923a13ff0af5a4542c025fae5
Karol <petitess@yahoo.com>
2024-05-26 15:47:27 +0200

    Github Backup

--- github-backup-01/github-backup.ps1
+++ github-backup-01/github-backup.ps1
@@ -1,12 +1,21 @@
+param (
+    [Parameter(Mandatory)]
+    [String]$GITHUB_TOKEN
+
+    # [ValidateSet('validate', 'what-if', 'create')]
+    # [String]$Command = 'create'
+)
+
+
 $Token
 $Org = "petitess"
 
 
-$URL = "https://api.github.com/orgs/$Org/repos"
+$URL = "https://api.github.com/repositories"
 $headers = @{
-    "Authorization"        = "Bearer $Token"
-    "Content-type"         = "application/json"
+    "Authorization"        = "Bearer $GITHUB_TOKEN"
     "X-GitHub-Api-Version" = "2022-11-28"
+    "Accept"               = "application/vnd.github+json"
 }
 $I = (Invoke-RestMethod -Method GET -URI $URL -Headers $headers) | ConvertTo-Json
 $I
