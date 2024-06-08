d32fa8e79b3dacfd64a23eb0a43ac4952ad1f9ee
Karol <93782725+petitess@users.noreply.github.com>
2024-06-08 04:45:31 +0200

    Delete terraform directory

--- terraform/README.md
+++
@@ -1,1 +0,0 @@
-https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/service_principal_oidc
--- terraform/main.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  prefix   = "abc-analytics-${var.env}-westeurope"
-  stPrefix = "abc${var.env}weu"
-}
-
-data "azurerm_resource_group" "rg" {
-  name                = "rg-infra-01"
-}
