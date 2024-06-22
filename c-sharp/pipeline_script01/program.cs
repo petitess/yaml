8242f546ff1be3ab956d625cf12800eab51710ef
Karol <93782725+petitess@users.noreply.github.com>
2024-06-22 15:11:37 +0200

    set & use environment variables in c#, devops

---
+++ c-sharp/pipeline_script01/program.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+
+namespace ConsolePipeline
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? MyName = Environment.GetEnvironmentVariable("MY_NAME_PWSH");
+            Console.WriteLine("Hello, " + MyName);
+            Console.WriteLine("CurrentDirectory: " + Environment.CurrentDirectory);
+            Console.WriteLine("Platform: " + Environment.OSVersion.Platform);
+            Console.WriteLine("KAROL_OIDC: " + Environment.GetEnvironmentVariable("KAROL_OIDC"));
+            Console.WriteLine("MY_NAME_BASH: " + Environment.GetEnvironmentVariable("MY_NAME_BASH"));
+            Console.WriteLine("MY_NAME_PWSH: " + Environment.GetEnvironmentVariable("MY_NAME_PWSH"));
+
+            foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
+                Console.WriteLine("  {0} = {1}", de.Key, de.Value);
+
+        }
+    }
+}
